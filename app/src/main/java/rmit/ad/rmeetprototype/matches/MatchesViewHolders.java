6d209193fa3455df53ab725ed6a4a567c5ab172a
Billy <alexandernguyenbill@gmail.com>
2024-01-24 02:56:50 +0700

    Added my project

---
+++ app/src/main/java/rmit/ad/rmeetprototype/matches/MatchesViewHolders.java
@@ -0,0 +1,35 @@
+package rmit.ad.rmeetprototype.matches;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import rmit.ad.rmeetprototype.Chat.ChatActivity;
+import rmit.ad.rmeetprototype.R;
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
